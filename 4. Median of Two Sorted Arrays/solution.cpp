fc99ba7b8ad22c3426ccb0d1de9f6f821a0c115b
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-09-12 21:44:03 +0530

    Update solution.cpp

---
+++ 4. Median of Two Sorted Arrays/solution.cpp
@@ -0,0 +1,50 @@
+/*
+Runtime: 63 ms
+Memory Usage: 90 MB
+*/
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        //1. Merging two sorted arrays in third array
+        vector<int> nums3;
+        int i,j,k;
+        i=j=k=0;
+        while(i<nums1.size() && j< nums2.size()){
+            if(nums1[i]<=nums2[j]){
+                nums3.push_back(nums1[i]);
+                i++;
+            }
+            else if(nums1[i]> nums2[j]){
+                nums3.push_back(nums2[j]);
+                j++;
+            }
+            k++;
+        }
+        if(i<nums1.size()){
+            while(i<nums1.size()){
+                nums3.push_back(nums1[i]);
+                i++;
+            }
+        }
+        if(j< nums2.size()){
+            while(j<nums2.size()){
+                nums3.push_back(nums2[j]);
+                j++;
+            }
+        }
+        
+        //2. Finding median;
+        double median = 0;
+        int s=0;
+        int e = nums3.size() - 1;
+        int m = s + (e-s)/2;
+        if(nums3.size()%2==0){ // Even length array
+            median = (nums3[m]+nums3[m+1])/2.0;
+        }
+        else{ //Odd length array
+            median = nums3[m];
+        }
+        return median;
+    }
+};
