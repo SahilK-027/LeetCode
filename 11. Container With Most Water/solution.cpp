5795f47cdc6ed7a5c83f21f637757db4861855e6
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-09-13 20:49:55 +0530

    Update solution.cpp

---
+++ 11. Container With Most Water/solution.cpp
@@ -0,0 +1,29 @@
+/*
+Runtime: 107 ms
+Memory Usage: 59 MB
+*/
+//Two pointer approach
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int s = 0;
+        int e = height.size()-1;
+        int ans=0;
+        int minimum;
+        while(s<e){
+            int dist = e-s;
+            minimum = min(height[e],height[s]);
+            if(minimum * dist > ans)
+            {
+                ans = minimum * dist;
+            }
+            if( height[s]<=height[e]){
+                s++;
+            }
+            else if(height[s]>height[e]){
+                e--;
+            }
+        }
+        return ans;
+    }
+};
