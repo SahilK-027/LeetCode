415414e70e9dd6eabd7ebe228edc484720cbb29e
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-09-17 10:04:04 +0530

    Update solution.cpp

---
+++ 19. Remove Nth Node From End of List/solution.cpp
@@ -0,0 +1,84 @@
+/*
+Runtime: 5 ms
+Memory Usage: 10.8 MB
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    void deleteFirstNode(ListNode *&start)
+    {
+        if (start == NULL)
+        {
+        return;
+        }
+        ListNode *temp;
+        temp = start;
+        start = temp->next;
+        delete temp;
+    }
+    void deleteLastnode(ListNode* &start)
+    {
+        if (start == NULL){
+            return;
+        }
+        ListNode *temp1;
+        ListNode *temp2;
+        temp1 = start;
+        temp2 = NULL;
+        while (temp1->next != NULL){
+            temp2 = temp1;
+            temp1 = temp1->next;
+        }
+        if (temp2){
+            temp2->next = NULL;
+            delete temp1;
+            return;
+        }
+        start = NULL;
+        delete temp1;
+    }
+    ListNode* delete_Kth_Node(ListNode*& head,int n){
+        ListNode* temp= head;
+        int length = 1;
+        while(temp != NULL){
+            length++;
+            temp = temp->next;
+        }
+        int k = length - n;
+        int x =1;
+        ListNode* t1= head;
+        ListNode* t2= NULL;
+        while(x!=k){
+            t2 =t1;
+            t1 =t1->next;
+            x++;
+        }
+        if(t2){
+            t2->next = t1->next;
+            delete t1;
+        }
+        else{
+            deleteFirstNode(head);
+        }
+        return head;
+    }
+    
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(n==1){
+            deleteLastnode(head);
+            return head;
+        }  
+        return delete_Kth_Node(head,n);
+    }
+};
