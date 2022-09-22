cf6f329c6b7b98aca9b4942f5d333b50953fd875
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-09-22 19:24:38 +0530

    Update solution.cpp

---
+++ 50. Pow(x, n)/solution.cpp
@@ -0,0 +1,39 @@
+/*
+Runtime: 3 ms
+Memory Usage: 6.2 MB
+*/
+class Solution {
+public:
+    double myPow(double x, int n) {
+    long long num = n;
+    double partial_ans;  //Using recurtion
+        
+    if (x == 0)     // In pow(x,n) if x=0 ,ans=0
+        return 0;
+    if (num == 0)
+        return 1;  // If power is zero ans is 1
+    
+    //Fast exponential method
+    //If power is negative calculate for positive power ans and then return 1/ans
+    if (num < 0)
+    {
+        long long b = -1 * num;
+        if (b % 2 == 0) // Even
+            partial_ans = pow(x, b / 2) * pow(x, b / 2);
+        else
+            partial_ans = x * pow(x, b / 2) * pow(x, b / 2);
+        return 1/(partial_ans);
+    }
+    
+    //Else power is positive or 0
+    else
+    {
+        int b = num;
+        if (b % 2 == 0) // Even
+            partial_ans = pow(x, b / 2) * pow(x, b / 2);
+        else
+            partial_ans = x * pow(x, b / 2) * pow(x, b / 2);
+    }
+    return partial_ans;
+    }
+};
