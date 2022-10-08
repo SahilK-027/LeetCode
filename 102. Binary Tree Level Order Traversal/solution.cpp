76cea2e9caa0d2a45b2499fc0b7eb9f90f535252
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-10-08 10:47:08 +0530

    Update solution.cpp

---
+++ 102. Binary Tree Level Order Traversal/solution.cpp
@@ -0,0 +1,56 @@
+// Runtime: 0 ms
+// Memory Usage: 12.5 MB
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == NULL ){
+            return ans;
+        }
+        queue<TreeNode *> q;
+        q.push(root);
+        q.push(NULL);
+        vector<int> v;
+        while (!q.empty())
+        {
+            TreeNode *temp = q.front();
+            q.pop();
+            if (temp != NULL)
+            {
+                v.push_back(temp->val);
+                if (temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if (temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+            else
+            {
+                // 1 level has completely traversed
+                ans.push_back(v);
+                v.clear();
+                if (!q.empty())
+                {
+                    // Queue still has elements in next level
+                    q.push(NULL);
+                }
+            }
+        }
+        return ans;
+    }
+};
