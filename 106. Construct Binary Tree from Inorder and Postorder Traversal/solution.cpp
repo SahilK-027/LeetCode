d1ec0d68f87df252ad0106a933db0ee1930ad28e
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-10-08 11:02:25 +0530

    Update solution.cpp

---
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal/solution.cpp
@@ -0,0 +1,55 @@
+// Runtime: 130 ms
+// Memory Usage: 270.1 MB
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int find_Position(vector<int> in, int element){
+        int n = in.size();
+        for(int i = 0 ; i< n ;i++){
+            if(in[i] == element){
+                return i;
+            }
+        }
+        return -1;
+    }
+    TreeNode* solve(vector<int>post , vector<int> in, int& root_index , int start, int end, int size){
+    // Base case
+    if(root_index < 0 || start > end){
+        return NULL;
+    }
+    // Find root element from preorder traversal
+    int element = post[root_index--];
+    TreeNode* root = new TreeNode(element);
+    
+    // Find root element's position from inorder traversal
+    int position = find_Position(in, element);
+    
+    // Call for left subtree and right subtree
+    root->right = solve(post, in, root_index, position+1, end, size);
+    root->left = solve(post, in, root_index, start, position-1,size);
+    
+    return root;
+}
+public:
+    TreeNode* buildTree(vector<int>& in, vector<int>& post) 
+    {
+        int post_size = post.size();
+        int in_size = in.size();
+        int root_index = post_size-1;
+        int in_start = 0;
+        int in_end = in_size-1;
+        TreeNode* root = solve(post, in, root_index, in_start, in_end, post_size);
+        return root;
+    }
+};
