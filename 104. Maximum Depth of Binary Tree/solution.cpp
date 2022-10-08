97c92016affa0a332b5e8a4832e01b798fd7b404
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-10-08 10:54:42 +0530

    Update solution.cpp

---
+++ 104. Maximum Depth of Binary Tree/solution.cpp
@@ -0,0 +1,35 @@
+// Runtime: 15 ms
+// Memory Usage: 18.9 MB
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {    
+public:
+    int maxDepth(TreeNode* root) {
+        // Base Case
+        if(root == NULL){
+            // If no node in tree return 0 as height
+            return 0;
+        }
+        // Recursion will find max depth of left subtree
+        int max_depth_left = maxDepth(root->left);
+        
+        // Recursion will find max depth of right subtree
+        int max_depth_right = maxDepth(root->right);
+        
+        // Our job is to add root to max height
+        int max_depth = max(max_depth_left, max_depth_right);
+        int ans = max_depth + 1; 
+        
+        return ans;
+    }
+};
