60b7656381ffb749ab6d824159f6d0ebd9a63be2
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-10-08 09:50:31 +0530

    Update solution.cpp

---
+++ 83. Remove Duplicates from Sorted List/solution.cpp
@@ -0,0 +1,37 @@
+// Runtime: 16 ms
+// Memory Usage: 11.8 MB
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* curr = head;
+        while(curr->next != NULL){
+            if(curr->val == curr->next->val){
+                curr->next = curr->next->next;
+            }
+            else{
+                curr->next = curr->next;
+                curr = curr->next;
+                curr->next = curr->next;
+            }
+        }
+        curr->next = curr->next;
+        return head;
+    }
+};
