ea6bb12b93cd19931e7f179b41b692ec8c56fcc2
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-10-08 10:05:03 +0530

    Update solution.cpp

---
+++ 88. Merge Sorted Array/solution.cpp
@@ -0,0 +1,32 @@
+// Runtime: 8 ms
+// Memory Usage: 9.2 MB
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0;
+        int j = 0;
+        while(i<nums1.size() && j< n){
+            if(nums1[i]<=nums2[j] && i!=m){
+                i++;
+            }
+            if(nums1[i]> nums2[j] && i!=m)
+            {
+                for(int x = m-1;x>=i;--x){
+                    nums1[x+1] = nums1[x];
+                }
+                nums1[i]=nums2[j];
+                i++;
+                j++;
+                m++;
+            }
+            if(i==m){
+                while(j<n){
+                    nums1[i]=nums2[j];
+                    i++;
+                    j++;
+                }
+            }
+        }
+    }
+};
