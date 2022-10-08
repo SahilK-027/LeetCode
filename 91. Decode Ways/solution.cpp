8616dba5de762f1d81fff4ff5f648216c63fe2a6
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-10-08 10:28:27 +0530

    Update solution.cpp

---
+++ 91. Decode Ways/solution.cpp
@@ -0,0 +1,47 @@
+// Runtime: 0 ms
+// Memory Usage: 6.2 MB
+
+// Optimized recursive solution
+// TC - O(N)
+
+
+class Solution {
+private:
+    int solve(string &s, int i, int* dp){
+        if(i >= s.size()){
+            return 1;
+        }
+        // IF dp[i] is already calculated then retun it no need to calculate again
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        // else calculate answer
+        int ans = 0;
+        int path_1 = s[i] - '0'; // taking out 1 from "11106" to solve
+        int path_2 =0;
+        if( i < s.length() - 1){
+            path_2 = path_1 *10 + s[i+1] - '0' ;//taking out 11 from "11106 " to solve
+        }
+        if(path_1 > 0){
+            ans = ans + solve(s, i+1,dp);
+        }
+        if(path_1 != 0 && path_2 > 0 && path_2 <= 26){
+            ans = ans + solve(s,i+2,dp);
+        }
+        // store answer in dp
+        dp[i] = ans;
+        return ans;    
+    }
+public:
+    int numDecodings(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        
+        // TO STORE INTERMIDIATE ANSWERS WE WILL CREATE AN DP
+        int dp[101];
+        memset(dp,-1,sizeof(dp));
+        
+        int ans = solve(s,0, dp);
+        return ans;
+    }
+};
