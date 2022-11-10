9ac9d93085d71576e7a3b80ac9f813a62139ea83
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-11-10 21:06:39 +0530

    Add files via upload

---
+++ 108. Convert Sorted Array to Binary Search Tree/README.md
@@ -0,0 +1,25 @@
+# Convert Sorted Array to Binary Search Tree
+## Description
+
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+ 
+
+Example 1:
+
+![alt](https://assets.leetcode.com/uploads/2021/02/18/btree1.jpg)
+```
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+![alt](https://assets.leetcode.com/uploads/2021/02/18/btree2.jpg)
+```
+Example 2:
+
+![alt](https://assets.leetcode.com/uploads/2021/02/18/btree.jpg)
+```
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+```
---
+++ 108. Convert Sorted Array to Binary Search Tree/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    TreeNode* solve(int s, int e, vector<int>& v){
+        if(s > e){
+            return NULL;
+        }
+        int m = (s + e)/2;
+        TreeNode* root = new TreeNode(v[m]);
+        root -> left = solve(s,m-1,v);
+        root-> right = solve(m+1,e,v);
+        return root;
+    }
+public:
+    TreeNode *sortedArrayToBST(vector<int> &v)
+    {
+        int s = 0;
+        int e = v.size()-1;
+        TreeNode* ans = solve(s,e,v);
+        return ans;
+    }
+};
