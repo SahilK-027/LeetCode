d1c04ce1b2aa5b2e78ea3066b5ea2b595fe354bd
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-11-10 21:13:57 +0530

    Add files via upload

---
+++ 112. Path Sum/README.md
@@ -0,0 +1,35 @@
+# Path Sum
+## Description
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+Example 1:
+
+![alt](https://assets.leetcode.com/uploads/2021/01/18/pathsum1.jpg)
+```
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+```
+Example 2:
+
+![alt](https://assets.leetcode.com/uploads/2021/01/18/pathsum2.jpg)
+```
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+```
+Example 3:
+
+```
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+```
---
+++ 112. Path Sum/solution.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool solve(TreeNode* root, int targetSum){
+        // Base case
+        if(root == NULL){
+            return false;
+        }
+        //  Processing
+        targetSum = targetSum - root->val;
+        
+        // If we reached the leaf node and our target sum becomes 0
+        if(targetSum == 0 && root->left == NULL && root->right == NULL){
+            return true;
+        }
+        
+        // Recursive call
+        bool leftAns = solve(root->left,targetSum);
+        bool rightAns = solve(root->right,targetSum);
+        
+        // Answer either exists in left subtree or right subtree
+        return (leftAns || rightAns);
+    }
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        bool ans = solve(root, targetSum);
+        return ans;
+    }
+};
