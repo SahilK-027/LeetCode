3082c079a2129cc5ce1e7889a443a0a93f4d53eb
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-11-11 15:03:14 +0530

    Add files via upload

---
+++ 167. Two Sum II - Input Array Is Sorted/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int t) {
+        vector<int> ans;
+        int s = 0;
+        int e = arr.size()-1;
+        while(s<e){
+            if(arr[s] + arr[e] == t){
+                // As for question indexing starts from 1
+                ans.push_back(s+1);
+                ans.push_back(e+1);
+                break;
+            }
+            else if(arr[s] + arr[e] < t){
+                s++;
+            }
+            else{
+                e--;
+            }
+        }
+        return ans;
+    }
+};
