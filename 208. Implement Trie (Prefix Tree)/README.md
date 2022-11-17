bd7e394c2f3ad24a35196379d4f35a7a2be6310c
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-11-17 19:12:44 +0530

    Add files via upload

---
+++ 208. Implement Trie (Prefix Tree)/README.md
@@ -0,0 +1,30 @@
+# Implement Trie (Prefix Tree)
+## Description
+
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+- Trie() Initializes the trie object.
+- void insert(String word) Inserts the string word into the trie.
+- boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+- boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+
+Example 1:
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+```
---
+++ 208. Implement Trie (Prefix Tree)/solution.cpp
@@ -0,0 +1,112 @@
+class Trienode{
+public:
+    char data;
+    Trienode* children[26];
+    bool isTerminal;
+
+    Trienode(char ch){
+        this->data = ch;
+        for(int i = 0; i < 26; i++){
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie {
+public:
+    Trienode* root;
+    Trie() {
+        root = new Trienode('\0');
+    }
+    
+    // Insertion of word
+    void insertUtil(Trienode* root, string word){
+        // Base case
+        if(word.length() == 0){
+            root->isTerminal = true;
+            return;
+        }
+
+        // Assume word will be in lower case
+        int index = word[0] - 'a';
+        Trienode* child;
+
+        // If already present move ahead
+        if(root->children[index]  != NULL){
+            child = root->children[index];
+        }else{
+            // If Absent create and attach
+            child = new Trienode(word[0]);
+            root->children[index] = child;
+        }
+
+        // Recursion
+        insertUtil(child, word.substr(1));
+    }
+    void insert(string word) {
+        insertUtil(root, word);
+    }
+    
+
+    // Searching word
+    bool searchUtil(Trienode* root, string word){
+        // Base case
+        if(word.length() == 0){
+            if(root->isTerminal){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        int index = word[0] - 'a';
+        Trienode* child;
+
+        // WORD PRESENT
+        if(root->children[index] != NULL){
+             child = root->children[index];
+        }else{
+            return false;
+        }
+
+        // Recursion
+        return searchUtil(child, word.substr(1));
+    }
+    bool search(string word) {
+        return searchUtil(root, word);
+    }
+    
+
+
+    bool startsWithUtil(Trienode* root, string word){
+        // Base case
+        if(word.length() == 0){
+            return true;
+        }
+
+        int index = word[0] - 'a';
+        Trienode* child;
+
+        // WORD PRESENT
+        if(root->children[index] != NULL){
+             child = root->children[index];
+        }else{
+            return false;
+        }
+
+        // Recursion
+        return startsWithUtil(child, word.substr(1));
+    }
+    bool startsWith(string word) {
+        return startsWithUtil(root, word);
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
