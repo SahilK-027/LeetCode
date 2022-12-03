28071b27efe96acd6414867b330e80574f40888d
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-03 11:18:15 +0530

    Add files via upload

---
+++ 451. Sort Characters By Frequency/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        string ans;
+        // To store number and its occurance
+        unordered_map<char, int>mp;
+        for(int i= 0 ; i < s.length(); i++){
+            if(mp.find(s[i]) == mp.end()){
+                mp[s[i]] = 1;
+            }
+            else{
+                mp[s[i]]++;
+            }
+        }
+        vector<pair<int,char>> v;
+        for(auto i: mp){
+            v.push_back(make_pair(i.second,i.first));
+        }
+        sort(v.begin(), v.end());
+        for (int i= v.size()-1; i >= 0; i--)
+        {
+            while(v[i].first--){
+                ans.push_back(v[i].second);
+            }
+        }
+        return ans;
+    }
+};
