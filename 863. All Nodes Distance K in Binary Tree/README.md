02f0c5f9539beaf876231f99d50a64b55f806779
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-04 16:34:50 +0530

    Add files via upload

---
+++ 863. All Nodes Distance K in Binary Tree/README.md
@@ -0,0 +1,22 @@
+# All Nodes Distance K in Binary Tree
+## Description
+
+Given the root of a binary tree, the value of a target node target, and an integer k, return an array of the values of all nodes that have a distance k from the target node.
+
+You can return the answer in any order.
+
+Example 1:
+
+![alt](https://s3-lc-upload.s3.amazonaws.com/uploads/2018/06/28/sketch0.png)
+```
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, k = 2
+Output: [7,4,1]
+Explanation: The nodes that are a distance 2 from the target node (with value 5) have values 7, 4, and 1.
+```
+
+Example 2:
+
+```
+Input: root = [1], target = 1, k = 3
+Output: []
+```
---
+++ 863. All Nodes Distance K in Binary Tree/solution.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    set<TreeNode*> visited;
+    void create_mapping(TreeNode* root, unordered_map < TreeNode* , TreeNode* >&mp){
+        if(root == NULL){
+            return;
+        }
+        if(root->left){
+            mp[root->left] = root;
+            create_mapping(root->left, mp);
+        }
+        if(root->right){
+            mp[root->right] = root;
+            create_mapping(root->right, mp);
+        }
+    }
+    void traverseInK(TreeNode* root, int k,  unordered_map < TreeNode* , TreeNode* > nodeToParent,vector<int>& ans){
+        // Keeping track of already visited nodes
+        if( visited.find(root) != visited.end() )
+            return;
+        visited.insert(root);
+
+        if(k <= 0 ){
+            ans.push_back(root->val);
+            return;
+        }
+        // Traverse in all direction
+        if(root->left){
+            traverseInK(root->left, k-1, nodeToParent ,ans);
+        }
+        if(root->right){
+            traverseInK(root->right, k-1, nodeToParent ,ans);
+        }
+        if(nodeToParent[root]){
+            traverseInK(nodeToParent[root], k-1, nodeToParent ,ans);
+        }
+    }
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* t, int k){
+        int val = t->val;
+        // Step 1: Create node to parent mapping
+        unordered_map < TreeNode* , TreeNode* > nodeToParent;
+        nodeToParent[root] = NULL;
+        create_mapping(root, nodeToParent);
+        vector<int> ans;
+        // Step 2: Traverse in k steps and store ans at kth step
+        traverseInK(t, k ,nodeToParent, ans);
+        return ans;
+    }
+};
