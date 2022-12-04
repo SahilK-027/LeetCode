fe42dabb8885ea44fdbc02e37659aa3cf10f6e18
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-04 11:46:57 +0530

    Add files via upload

---
+++ 233. Number of Digit One/README.md
@@ -0,0 +1,19 @@
+# Number of Digit One
+## Description
+
+Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
+
+
+Example 1:
+
+```
+Input: n = 13
+Output: 6
+```
+
+Example 2:
+
+```
+Input: n = 0
+Output: 0
+```
---
+++ 233. Number of Digit One/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    int getlen(int n){
+        int length = 0;
+        while(n){
+            n /= 10;
+            length++;
+        }
+        return length;
+    }
+    int solve(int n){
+        // Number of 1 in 0 are 0
+        if(n <= 0) return 0;
+        //  Number of 1 in  10 - 0 are 1
+        if(n < 10) return 1;
+        int length = getlen(n);
+        int base = pow(10,length-1);
+        int first_digit = n / base;
+        int rem = n % base;
+        int oneInBase = 0;
+        if(first_digit == 1)
+            oneInBase = n - base + 1;
+        else
+            oneInBase = base;
+        return solve(rem) + first_digit * solve(base -1) + oneInBase;
+    }
+public:
+    int countDigitOne(int n) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        return solve(n);
+    }
+};
