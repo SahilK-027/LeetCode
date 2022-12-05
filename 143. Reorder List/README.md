e14b999efa1d8be8406673508620e3dffc53efb1
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-05 22:47:24 +0530

    Add files via upload

---
+++ 143. Reorder List/README.md
@@ -0,0 +1,30 @@
+# Number of Steps to Reduce a Number to Zero
+## Description
+
+You are given the head of a singly linked-list. The list can be represented as:
+
+```
+L0 → L1 → … → Ln - 1 → Ln
+```
+Reorder the list to be on the following form:
+
+```
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+```
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Example 1:
+
+![alt](https://assets.leetcode.com/uploads/2021/03/04/reorder1linked-list.jpg)
+```
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+```
+
+Example 2:
+
+![alt](https://assets.leetcode.com/uploads/2021/03/09/reorder2-linked-list.jpg)
+```
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+```
---
+++ 143. Reorder List/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        // Only one element list
+        if(head->next == NULL){
+            return;
+        }
+        // Onl;y two element list
+        if(head->next->next == NULL){
+            return;
+        }
+
+        // Find out last and second last elements
+        ListNode* temp1 = head;
+        ListNode* temp2 = NULL;
+        while(temp1->next != NULL){
+            temp2 = temp1;
+            temp1 = temp1->next;
+        }
+
+        // Findout next head, Do the pointer manipulation and ask recursion to solve remaining LL
+        ListNode* next_head = head->next;
+        temp1->next = next_head;
+        temp2->next = NULL;
+        head->next = temp1;
+        reorderList(next_head);
+    }
+};
