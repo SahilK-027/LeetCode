eb6cde9181575784c4a5fc32f8ac318beec8fb91
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-06 20:13:52 +0530

    Create solution2.cpp

---
+++ 1863. Sum of All Subset XOR Totals/solution2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    void solve(vector<int>& arr, int& grand_total, int xorAns, int i =0){
+        if(i == arr.size()){
+            grand_total += xorAns;
+            return;
+        }
+        // Include
+        solve(arr, grand_total,xorAns ^ arr[i], i+1);
+        // Exclude
+        solve(arr, grand_total,xorAns, i+1);
+    }
+public:
+    int subsetXORSum(vector<int>& arr) {
+        // Step 1:
+        int grand_total = 0;
+        int xorAns = 0;
+        solve(arr,grand_total,xorAns);
+        return grand_total;
+    }
+};
