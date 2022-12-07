b9aa5be2153ba77a60de70ce3b059ddb7424b264
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-07 07:28:08 +0530

    Add files via upload

---
+++ 938. Range Sum of BST/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void solve(TreeNode* root, int low, int high, int& sum){
+        if(root == NULL) return;
+        if(root->val >= low && root->val <= high) sum+= root->val;
+        solve(root->left, low, high, sum);
+        solve(root->right, low, high, sum);
+    }
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+        solve(root, low, high, sum);
+        return sum;
+    }
+};
