bf827945a3318b84980d521d64166c204d9f1911
Shashank Bhosagi <78866224+shashankbhosagi@users.noreply.github.com>
2022-12-08 20:01:26 +0530

    Create solution2.cpp
    I have used loops here. As the max value of INT is 2^31 - 1, so I have ran the loop till 2^30 and checked at each iteration..
    Please accept my solution and if any changes please tell me ⊙.☉

---
+++ 231. Power of Two/solution2.cpp
@@ -0,0 +1,25 @@
+/*
+Runtime - 0ms
+Memory - 5.9MB
+*/
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+
+        int ans = 1;
+        for (int i = 0; i <= 30; i++)
+        {
+
+            if (ans == n)
+            {
+                return true;
+            }
+
+            if (ans < INT_MAX / 2)
+                ans = ans * 2;
+        }
+        return false;
+    }
+};
