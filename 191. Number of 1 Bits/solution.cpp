c8dff152ec8b9b8dcd273018b836b4f767bd61f4
Shashank Bhosagi <78866224+shashankbhosagi@users.noreply.github.com>
2022-12-08 21:06:28 +0530

    Done suggested changes
    Added a comment

--- 191. Number of 1 Bits/solution.cpp
+++ 191. Number of 1 Bits/solution.cpp
@@ -6,7 +6,7 @@ public:
             int bit = n % 2;  //Convert the number to binary
             if(bit)
                 set_bit_count++; //If bit == 1 count++
-            n=n/2;
+            n=n/2; //This can be done alternately using right shift operator x>>y = x/2y
         }
         return set_bit_count;
     }
