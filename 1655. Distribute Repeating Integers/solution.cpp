960d4025775cf577eb55c0392fbd6bb4d1fa31c2
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-10 13:42:53 +0530

    Add files via upload

---
+++ 1655. Distribute Repeating Integers/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool solve(vector<int>& qty,map<int,int>& mp, int i ) {
+        //base case
+        if(i >= qty.size()) {
+            // if we have traversed quantity comp,etely means all customers are statisfied
+            return true;
+        }
+        
+        for(auto it = mp.begin(); it != mp.end(); ++it) {
+            if(it->second >= qty[i]) {
+                
+                mp[it->first] -= qty[i];
+                
+                bool recAns = solve(qty, mp, i+1);
+                if(recAns)
+                    return true;
+                else
+                //BT
+                mp[it->first] += qty[i];
+            }
+        }   
+      return false;  
+    }
+    
+    bool canDistribute(vector<int>& arr, vector<int>& qty) {
+        // Step 1: To store numand its count
+        map<int,int> mp;
+        for(int i=0; i<arr.size(); i++) {
+            mp[arr[i]]++;
+        }
+        //step2: sort quantity desc
+        sort(qty.begin(), qty.end(), greater<int>());
+        
+        return solve(qty, mp, 0);
+    }
+};
