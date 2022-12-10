91d45b4e9b9c8b651af860ce03743aae8f8a5b9a
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-10 12:36:34 +0530

    Add files via upload

---
+++ 1339. Maximum Product of Splitted Binary Tree/solution.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ // rem_subtree_sum = total_tree_sum - curr_subtree_sum
+class Solution {
+private:
+    // Function to calculate total sum of tree
+    void tot_sum(TreeNode*& root, long &total_sum){
+        // Base case
+        if(root == NULL){
+            return;
+        }
+        // Processing
+        total_sum += root->val;
+        // Recursive calls
+        tot_sum(root->left, total_sum);
+        tot_sum(root->right, total_sum);
+    }
+
+    int solve(TreeNode* root, long& maxp, long& total_sum){
+        // Base case
+        if(root == NULL){
+            return 0;
+        }
+        // Find curr_subtree_sum 
+        long left_sum = solve(root->left, maxp, total_sum);
+        long right_sum = solve(root->right, maxp, total_sum);
+        long curr_subtree_sum = root->val + left_sum + right_sum;
+        // Find remaining subree sum
+        long rem_subtree_sum = total_sum - curr_subtree_sum;
+        // Update max
+        maxp = max(maxp, curr_subtree_sum * rem_subtree_sum);
+        // We are returning curr_subtree_sum
+        return curr_subtree_sum;
+    }
+public:
+    int maxProduct(TreeNode* root) {
+        // Calculate total sum
+        long total_sum = 0;
+        tot_sum(root, total_sum);
+        // Solve 
+        long maxp = 0;
+        solve(root, maxp, total_sum);
+        // Since the answer may be too large, return it modulo 10^9 + 7.
+        return (maxp % 1000000007);
+    }
+};
