11ca828f3cf8a70a7a71c2622208f0992bb03775
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-12 18:16:35 +0530

    Add files via upload

---
+++ 70. Climbing Stairs/solution.cpp
@@ -0,0 +1,41 @@
+/*
+* 0 ms
+* 6.2 mb
+*/
+/**
+ * @author : Sahilk-027
+ * @brief : Using next_free_space array
+*/
+
+// Inclusion exclusion logic
+class Solution {
+private:
+    int solve(int n, vector<int>& dp, int stair = 0){
+        // Base case if we reach nth stair then return 1..As there is 1 possible solution
+        if(stair == n){
+            return 1;
+        }
+        // If we reach stair above n then no answer exists so return 0
+        if(stair > n){
+            return 0;
+        }
+
+        if(dp[stair] != -1){
+            return dp[stair];
+        }
+        // Ans for 1 stair climb
+        int ans1 = solve(n, dp, stair+1);
+        // Ans for 2 stair climb
+        int ans2 = solve(n, dp, stair+2);
+        
+        // Final ans
+        int final_ans = ans1 + ans2;
+        dp[stair] = final_ans;
+        return final_ans;
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
+    }
+};
