67a4cb8c7ae75113de345a7f05f50c9e9a0a59aa
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-12-14 21:25:23 +0530

    Create solution4.cpp

---
+++ 509. Fibonacci Number/solution4.cpp
@@ -0,0 +1,21 @@
+// Tabularization + space optimization
+class Solution {
+public:
+    int solve(int n){
+        int prev2 = 0;
+        int prev1 = 1;
+        int curr;
+        for(int i = 2 ; i <= n ; i++){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
+    int fib(int n) {
+        if(n < 2){
+            return n;
+        }
+        return solve(n);
+    }
+};
