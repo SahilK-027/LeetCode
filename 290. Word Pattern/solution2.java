f59177184257eb0f5007d9ae5c8b9568638405e7
Yash Londhe <99034957+yashlondhe90960@users.noreply.github.com>
2023-01-01 20:01:46 +0530

    Update solution2.java
    made required changes

--- 290. Word Pattern/solution2.java
+++ 290. Word Pattern/solution2.java
@@ -26,21 +26,25 @@ public class Solution {
             Map<Character, String> map = new HashMap<>();
 
             for (int i = 0; i < pattern.length(); i++) {
-                char c = pattern.charAt(i);
+                char curr_char = pattern.charAt(i);  //to get the current character of pattern
                 String token = tokens[i];
 
                 // Check the one-one mapping
-                if (!map.containsKey(c) && !inverseMap.containsKey(token)) {
-                    map.put(c, token);
-                    inverseMap.put(token, c);
-                } else if (map.containsKey(c) && inverseMap.containsKey(token)) {
-                    String tokenToCheck = map.get(c);
+                if (!map.containsKey(curr_char) && !inverseMap.containsKey(token))//if it does'nt contain anything the char or word
+                {
+                   map.put(curr_char, token);//simply put it in map
+                    inverseMap.put(token, curr_char);
+                } else if (map.containsKey(curr_char) && inverseMap.containsKey(token)) {
+                    String tokenToCheck = map.get(curr_char);
                     char charToCheck = inverseMap.get(token);
 
-                    if (!tokenToCheck.equals(token) || c != charToCheck) {
-                        return false;
-                    }
-                } else { 
+         //checking if mapping of curr_char is equal to curr_word or not
+               if (!tokenToCheck.equals(token) || curr_char != charToCheck) 
+               {
+                        return false;  //it has to be equal so we returned false      
+                }
+                } else
+                { 
                     return false;
                 }
             }
