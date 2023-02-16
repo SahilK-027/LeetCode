a50750217c261da1e1f579f96312caeb4b03568f
ShubhamAsbe <76616652+ShubhamAsbe@users.noreply.github.com>
2023-02-16 22:35:54 +0530

    Update solution3.py

--- 1. Two_Sum/solution3.py
+++ 1. Two_Sum/solution3.py
@@ -12,6 +12,7 @@ SC : O(1)
 
 class Solution(object):
     def twoSum(self, nums, target):
+      #for each number in the array we will check there exist another which will sum up to give the target
       for i in range(len(nums)):
             for j in range(len(nums)):
                 if nums[i]+nums[j]==target and j!=i:
