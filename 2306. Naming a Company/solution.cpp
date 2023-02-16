5861f876ef857c612be1b54eb1b3571e5f6e677e
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-02-16 09:04:42 +0530

    Add files via upload

---
+++ 2306. Naming a Company/solution.cpp
@@ -0,0 +1,35 @@
+/*
+484 ms
+83.4 MB
+*/
+
+/**
+ * @author : SahilK-027
+ * @brief : Iterative
+ */
+
+class Solution
+{
+public:
+    long long distinctNames(vector<string> &ideas)
+    {
+        long long ans = 0;
+
+        // Group strings by initials
+        unordered_set<string> suffixes[26];
+
+        for (const string &idea : ideas)
+            suffixes[idea[0] - 'a'].insert(idea.substr(1));
+
+        for (int i = 0; i < 25; ++i)
+            for (int j = i + 1; j < 26; ++j)
+            {
+                int count = 0;
+                for (const string &suffix : suffixes[i])
+                    if (suffixes[j].count(suffix))
+                        ++count;
+                ans += 2 * (suffixes[i].size() - count) * (suffixes[j].size() - count);
+            }
+        return ans;
+    }
+};
