f079f9cabcd2358aacc9c3b79f65f73d273eedec
ShubhamAsbe <76616652+ShubhamAsbe@users.noreply.github.com>
2023-02-16 22:29:38 +0530

    Create solution3.py

---
+++ 1. Two_Sum/solution3.py
@@ -0,0 +1,18 @@
+"""
+4739 ms
+14.5 MB
+TC : O(N^2)
+SC : O(1)
+"""
+
+"""
+ * @author : ShubhamAsbe
+ * @brief : Iteration
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+      for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i]+nums[j]==target and j!=i:
+                    return [i,j]
