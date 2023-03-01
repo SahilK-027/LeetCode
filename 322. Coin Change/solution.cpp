08d99c01bdc0313a7a5104eeb3706b57f5d4e09d
Sahil Kandhare <sahilkandhare07@gmail.com>
2023-03-01 19:18:13 +0530

    Added: Tabulation solution to Coin change problem

--- 322. Coin Change/solution.cpp
+++ 322. Coin Change/solution.cpp
@@ -34,6 +34,27 @@ private:
         // Step 2: store the answer in DP before returning
         return dp[amount] = mini;
     }
+    // Using Tabulation 
+    int solveTab(vector<int>& coins, int amount){
+        vector<int>dp(amount+1 ,INT_MAX);
+        dp[0] = 0;
+
+        for(int i = 1 ; i <= amount; i++){
+            // Utilize all coins for current amount
+            for(auto c: coins){
+                // Checking for valid index
+                if(i - c >= 0 && dp[i - c] != INT_MAX){
+                    dp[i] = min(dp[i], 1 + dp[i - c]);
+                }
+            }
+        }
+        if(dp[amount] == INT_MAX){
+            return -1;
+        }
+        else{
+            return dp[amount];
+        }
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         if(amount == 0){
